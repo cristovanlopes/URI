844f124a876516ddf7700c271995d75a42209a46
cristovanlopes <50528087+cristovanlopes@users.noreply.github.com>
2019-09-21 18:23:33 -0300

    Create 1050.cpp

---
+++ 1050.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a;
+  cin>>a;
+  if(a == 61 || a ==71 || a==11 || a==21||a==32||a==19||a==27 ||a==31){
+    if(a==61){
+      cout<<"Brasilia"<<endl;
+    }
+    else if(a==71){
+      cout<<"Salvador"<<endl;
+    }
+    else if(a==11){
+      cout<<"Sao Paulo"<<endl;
+    }
+    else if(a==21){
+      cout<<"Rio de Janeiro"<<endl;
+    }
+    if(a==32){
+      cout<<"Juiz de Fora"<<endl;
+    }
+    else if(a==19){
+      cout<<"Campinas"<<endl;
+    }
+    else if(a==27){
+      cout<<"Vitoria"<<endl;
+    }
+    else if(a==31){
+      cout<<"Belo Horizonte"<<endl;
+    }
+  }
+  else{
+    cout<<"DDD nao cadastrado"<<endl;
+  }
+  return 0;
+}
