f5e66608d35138fd7a37e7021ae341034c440d5d
cristovanlopes <50528087+cristovanlopes@users.noreply.github.com>
2019-09-21 18:03:13 -0300

    Create 1049.cpp

---
+++ 1049.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+  string a, b, c;
+  cin >> a >> b >> c;
+
+  if(a == "vertebrado" && b == "ave" && c == "carnivoro"){
+    cout << "aguia" << endl;
+  }else if(a == "vertebrado" && b == "ave" && c == "onivoro" ){
+    cout<<"pomba"<<endl;}
+  else if(a=="vertebrado"&&b=="mamifero")
+  {
+    if(c=="onivoro"){
+      cout<<"homem"<<endl;}
+      else{
+        cout <<"vaca" << endl;
+      }
+    }
+  else{
+    if(b=="inseto"&&c=="hematofago"){cout<<"pulga"<<endl;}
+    else if(b=="inseto"&&c=="herbivoro"){cout<<"lagarta"<<endl;}
+    else{
+      if (c=="hematofago"){
+        cout << "sanguessuga" << endl;
+      }
+      else{
+        cout << "minhoca" << endl;
+      }
+    }
+  }
+}
